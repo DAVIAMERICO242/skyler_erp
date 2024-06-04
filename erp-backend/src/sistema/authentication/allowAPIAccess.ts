eb2a409e4021f559d539dd71d4709d6f8ca5aa5c
unknown <darodriguesufc@gmail.com>
2024-06-04 09:53:58 -0300

    middleware sicronizado por mutex

--- erp-backend/src/sistema/authentication/allowAPIAccess.ts
+++ erp-backend/src/sistema/authentication/allowAPIAccess.ts
@@ -4,19 +4,27 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const AuthMiddleware = (req: RequestModel,
+import {Mutex} from 'async-mutex';
+const mutex = new Mutex();
+
+export const AuthMiddleware = async (req: RequestModel,
     res: Response,
     next:NextFunction)=>{
 
+    const release = await mutex.acquire();
     const token = req?.headers['token'] as string;
+
     if(!token){
         res.status(400).end('ACESSO ILEGAL A API');
+        release();
     }else{
         try{
             const decoded:(JwtModel) = jwt.verify(token,process.env.JWT_SECRET as string) as JwtModel;
             req.username = decoded.username;
+            release();
             next();
         }catch(err){
+            release();
             res.status(400).end('ACESSO ILEGAL A API');
         }
     }
