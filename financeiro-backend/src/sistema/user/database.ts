c4688c9d0bb2235fbb12984586dc1cdb4fa987ec
unknown <darodriguesufc@gmail.com>
2024-06-24 14:47:00 -0300

    sql injection consertado

--- financeiro-backend/src/sistema/user/database.ts
+++ financeiro-backend/src/sistema/user/database.ts
@@ -1,20 +1,20 @@
 import { SQLConnection } from "../connect-sql";
 
-export function checkMatchingPass(username:string,password:string):Promise<null|any[]>{
-    return new Promise((resolve,reject)=>{
-        SQLConnection().then((connection)=>{
-            if (connection){
-                connection.query(`SELECT * FROM users WHERE username = '${username}'
-                AND password='${password}'`,(err,result)=>{
-                    if(err){
+export function checkMatchingPass(username: string, password: string): Promise<null | any[]> {
+    return new Promise((resolve, reject) => {
+        SQLConnection().then((connection) => {
+            if (connection) {
+                const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
+                connection.query(query, [username, password], (err, result) => {
+                    if (err) {
                         reject(null);
-                    }else{
+                    } else {
                         resolve(result);
                     }
-                })
+                });
             }
-        }).catch((err)=>{
+        }).catch((err) => {
             reject(err);
-        })
-    })
+        });
+    });
 }
